2b8d463a9beb65f31231788b10e5c7e72b9d7dae
Celia Baptiste <cfbaptiste@gmail.com>
2019-06-08 20:10:12 -0700

    try to get correct logic added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(function() {
-  var result;
   $("form#homes").submit(function(event)  {
     event.preventDefault();
 
     var things = $("input:radio[name=things]:checked").val();
 
-    if (things === "Don't have much use for a bunch of things..") {
+    if (things === "Don't have much use for a bunch of things...") {
       home = "Tiny Home";
+    } else if (things === "I've got to have the latest and greatest...")  {
+      home = "Mansion";
+    } else if (things === "I buy what I need when I need it...")  {
+      home = "Ranch Style Home"
+    } else if (things === "Whatever I need I can make myself...") {
+      home = "Log Cabin";
     }
-  }
-}
+
+    $("#result").show();
+
+
+  });
+});
