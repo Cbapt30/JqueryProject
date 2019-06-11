7587ec0786c7924fc69a6eb8e043fce4e3b2af22
Celia Baptiste <cfbaptiste@gmail.com>
2019-06-10 18:43:22 -0700

    fix button, add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 
 $(function() {
-  $("#homes form").submit(function()  {
-    event.preventDefault();
+  $("#homes").submit(function()  {
+        event.preventDefault();
 
     var things = $("input:radio[name=things]:checked").val();
-
+    var result;
 
     if (things === "Don't have much use for a bunch of things...") {
       result = "Tiny Home";
     } else if (things === "I've got to have the latest and greatest...")  {
       result = "Mansion";
     } else if (things === "I buy what I need when I need it...")  {
-      home = "Ranch Style Home"
+      result = "Ranch Style Home"
     } else if (things === "Whatever I need I can make myself...") {
-      home = "Log Cabin";
+      result = "Log Cabin";
     }
-    $("#home form").show(result);
+    console.log(result);
+    $("#output").text(result);
+    // $("#homes").hide();
+
   });
 });
